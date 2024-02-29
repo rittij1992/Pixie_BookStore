38d632ffa5b694b4e9ac57f9ff33bc09e3786259
rittij1992 <rittij.mazumder@gmail.com>
2024-02-29 19:55:45 +0530

    atlas connect reverse

--- back/configure/config_db.js
+++ back/configure/config_db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToDataBase = ()=>{
-    const url = 'mongodb+srv://rittijmazumder:iam-rittij1992@cluster0.rwttnzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/';
+    const url = 'mongodb://0.0.0.0:27017/store_db';
     return mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((()=> console.log('Connected to MongoDB!')))
     .catch(error => console.log('Could not connect to MongoDb', error))
